bec42b2bfc4d7ae170c09c1775c5067f44c13684
Tomasz Gosławski <tgoslawski@orione.no>
2024-11-21 18:01:44 +0100

    changes

--- ogr/ogrsf_frmts/sosi/ogrsosidatasource.cpp
+++ ogr/ogrsf_frmts/sosi/ogrsosidatasource.cpp
@@ -845,8 +845,8 @@ void OGRSOSIDataSource::buildOGRPoint(long iSerial)
     double dfEast = 0, dfNorth = 0, dfHeight = 0;
     dfHeight = LC_GetHoyde(1);
     LC_GetTK(1, &dfEast, &dfNorth);
-    CPLError(CE_Warning, CPLE_AppDefined, "height: %d", dfHeight);
-    CPLError(CE_Warning, CPLE_AppDefined, "east: %d", dfEast);
-    CPLError(CE_Warning, CPLE_AppDefined, "north: %d", dfNorth);
+    CPLError(CE_Warning, CPLE_AppDefined, "height: %lf", dfHeight);
+    CPLError(CE_Warning, CPLE_AppDefined, "east: %lf", dfEast);
+    CPLError(CE_Warning, CPLE_AppDefined, "north: %lf", dfNorth);
     papoBuiltGeometries[iSerial] = new OGRPoint(dfEast, dfNorth, dfHeight);
 }
